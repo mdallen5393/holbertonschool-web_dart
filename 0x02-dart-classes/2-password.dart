b92f82aabee004f9c6f337b725f9d18dc3fb03a3
mdallen5393 <allen_matthew@outlook.com>
2023-10-18 01:13:16 -0500

    Task 2

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -1,17 +1,17 @@
-import '2-password.dart';
+class Password {
+  String? password;
 
-void main() {
-  final ps = Password();
-  ps.password = "Just3z";
-  if (ps.isValid() == true)
-    print("${ps.password} is a Valid Password");
-  else
-    print("${ps.password} is Not a Valid Password");
-  print("${ps.toString()}");
-  ps.password = "ShouldWorkf7ne";
-  if (ps.isValid() == true)
-    print("${ps.password} is a Valid Password");
-  else
-    print("${ps.password} is Not a Valid Password");
-  print("${ps.toString()}");
+  bool isValid() {
+    if (password == null) return false;
+    if (password!.length < 8 || password!.length > 16) return false;
+    if (!password!.contains(new RegExp(r'[A-Z]'))) return false;
+    if (!password!.contains(new RegExp(r'[a-z]'))) return false;
+    if (!password!.contains(new RegExp(r'[0-9]'))) return false;
+    return true;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: ${password ?? "not set"}';
+  }
 }
