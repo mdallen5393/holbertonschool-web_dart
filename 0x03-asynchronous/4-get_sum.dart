b75ed86877992f318780f7269dc8f759564d7d2c
mdallen5393 <allen_matthew@outlook.com>
2023-10-18 16:59:26 -0500

    Task 4

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,20 @@
+import '4-util.dart';
+import 'dart:convert';
+
+Future<double> calculateTotal() async {
+  try {
+    Map userData = jsonDecode(await fetchUserData());
+    String userId = userData['id'];
+
+    List<dynamic> userOrder = jsonDecode(await fetchUserOrders(userId));
+
+    double total = 0.0;
+    for (var product in userOrder) {
+      total += double.parse(await fetchProductPrice(product));
+    }
+    return total;
+  } catch (error) {
+    print('error: $error');
+    return -1;
+  }
+}
