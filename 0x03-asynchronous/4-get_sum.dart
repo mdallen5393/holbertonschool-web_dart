30b31d5639db12c1f7af3e2a06a688269e5dcfdb
mdallen5393 <allen_matthew@outlook.com>
2023-10-18 17:12:13 -0500

    Task 4

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,19 +1,21 @@
-import '4-util.dart';
 import 'dart:convert';
+import '4-util.dart';
 
-Future<double> calculateTotal() async {
+Future<num> calculateTotal() async {
   try {
-    Map userData = jsonDecode(await fetchUserData());
-    String userId = userData['id'];
+    final String userData = await fetchUserData();
+    final String id = jsonDecode(userData)['id'];
 
-    List<dynamic> userOrder = jsonDecode(await fetchUserOrders(userId));
+    final String orderData = await fetchUserOrders(id);
+    final List orders = jsonDecode(orderData);
 
-    double total = 0.0;
-    for (var product in userOrder) {
-      total += double.parse(await fetchProductPrice(product));
-    }
-    return total;
-  } catch (error) {
+    final orderPrices = await Future.wait(orders
+        .map((order) async => double.parse(await fetchProductPrice(order))));
+
+    final double price =
+        orderPrices.reduce((sum, orderPrice) => sum + orderPrice);
+    return price;
+  } catch (err) {
     return -1;
   }
 }
